b05f7b86521bb345f11917480dab21d1cd0d8d01
muhammad.ilham <muhammad.ilham@divusi.com>
2024-07-23 11:55:26 +0700

    change server ip

--- inc/config.php
+++ inc/config.php
@@ -4,7 +4,7 @@ define('_ISONLINE', false);
 //-- assets folder
 define('_ASSET', 'assets/');
 //-- database configuration
-$dbhost = '194.163.42.176';
+$dbhost = '217.21.73.76';
 $dbuser = 'u1738039_papi';
 $dbpass = 'H*0azqD0!5T^';
 $dbname = 'u1738039_papi';
